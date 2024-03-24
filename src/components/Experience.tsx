ab9201ec3191f3c8bf87c9aa4753b567fde9866d
Christian Redfern <credfernbus@gmail.com>
2024-03-24 21:27:58 +0000

    Added messages

---
+++ src/components/Experience.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+    return (
+        <section id="projects">
+            <h2>My Projects</h2>
+            <ul>
+                <li>
+                    <h3>Project 1</h3>
+                    <p>Description of Project 1</p>
+                    {/* Add more details about the project, like images or links */}
+                </li>
+                <li>
+                    <h3>Project 2</h3>
+                    <p>Description of Project 2</p>
+                    {/* Add more details about the project, like images or links */}
+                </li>
+                {/* Add more project entries as needed */}
+            </ul>
+        </section>
+    );
+};
+
+export default Projects;
