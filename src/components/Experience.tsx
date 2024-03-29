0beb41969c6191f7da7823246fc67866ee684ee7
Christian Redfern <credfernbus@gmail.com>
2024-03-29 19:10:51 +0000

    Refactored further to prevent rerendering of heading and entries

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useMemo, useRef} from 'react';
 import i18n from 'i18next';
-import {initReactI18next, useTranslation} from 'react-i18next';
+import { initReactI18next, useTranslation } from 'react-i18next';
 import Messages from '../lang/en.tsx';
 
 i18n.use(initReactI18next).init(Messages);
 
-const Experience: React.FC = () => {
-    const { t } = useTranslation();
-
+const useShowBoxes = () => {
     const [showBoxes, setShowBoxes] = useState<boolean[]>([]);
     const boxRefs = useRef<(HTMLDivElement | null)[]>([]);
 
@@ -34,53 +32,65 @@ const Experience: React.FC = () => {
         boxRefs.current[index] = ref;
     };
 
-    const descriptionBoxes = useRef<HTMLDivElement[]>([]);
+    return { showBoxes, addBoxRef };
+};
+
+const useDescriptionBoxes = () => {
+    const { t } = useTranslation();
 
     useEffect(() => {
-        // Loop through all description box elements with the desired class
-        const elements = document.querySelectorAll(".description-box");
+        const descriptionBoxElements = document.querySelectorAll('.description-box');
+        descriptionBoxElements.forEach((element) => {
+            const button = element.querySelector('#timeline button');
+            const buttonFront = element.querySelector('#timeline .button_front');
 
-        elements.forEach((element) => {
-            const button = element.querySelector("#timeline button");
-            const buttonFront = element.querySelector("#timeline .button_front");
             if (button) {
                 const handleClick = () => {
-                    element.classList.toggle("active");
-                    button.classList.toggle("active_button");
-
-                    buttonFront.innerHTML = buttonFront.classList.contains("active_button")
+                    element.classList.toggle('active');
+                    button.classList.toggle('active_button');
+                    buttonFront.innerHTML = buttonFront.classList.contains('active_button')
                         ? t('experience.read-less')
                         : t('experience.read-more');
                 };
-                button.addEventListener("click", handleClick);
 
-                // Cleanup function to remove event listener on unmount
-                return () => button.removeEventListener("click", handleClick);
+                button.addEventListener('click', handleClick);
+
+                return () => button.removeEventListener('click', handleClick);
             }
         });
-    }, [descriptionBoxes.current]); // Dependency on descriptionBoxes
+    }, [t]);
+};
 
-    const years = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017]
+const Experience: React.FC = () => {
+    const { t } = useTranslation();
+    const { showBoxes, addBoxRef } = useShowBoxes();
+    useDescriptionBoxes();
 
-    const headings: string[] = []
-    const entries: string[] = []
-
-    years.forEach(year => {
-        headings.push(`experience.heading.${year}`);
-        entries.push(`experience.entry.${year}`);
-        console.log(year);
-    });
+    const headingsAndEntries = useMemo(() => {
+        const years = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017];
+        return years.reduce(
+            (acc, year) => {
+                acc.headings.push(`experience.heading.${year}`);
+                acc.entries.push(`experience.entry.${year}`);
+                return acc;
+            },
+            { headings: [], entries: [] }
+        );
+    }, []);
 
     return (
         <div className="experience-section" id="timeline">
             <ul className="timeline-ul">
-                {years.map((year, index) => (
+                {headingsAndEntries.headings.map((heading, index) => (
                     <li key={index}>
-                        <i className="experience-item">{year}</i>
-                        <div className={`box ${showBoxes[index] ? 'show' : ''}`} ref={(ref) => addBoxRef(ref, index)}>
-                            <h3 className="title">{t(headings[index])}</h3>
+                        <i className="experience-item">{index + 2017}</i>
+                        <div
+                            className={`box ${showBoxes[index] ? 'show' : ''}`}
+                            ref={(ref) => addBoxRef(ref, index)}
+                        >
+                            <h3 className="title">{t(heading)}</h3>
                             <div className="description-box">
-                                <p>{t(entries[index])}</p>
+                                <p>{t(headingsAndEntries.entries[index])}</p>
                                 <button className="button">
                                     <div className="button_front">read more</div>
                                 </button>
