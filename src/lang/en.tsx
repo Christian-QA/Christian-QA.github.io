54ba0c3aa7f542061c615dc932e5e3e1849c665a
Christian Redfern <credfernbus@gmail.com>
2024-03-29 18:34:44 +0000

    Added year message, looking for a way to abstract HTML

--- src/lang/en.tsx
+++ src/lang/en.tsx
@@ -13,7 +13,18 @@ const Messages = {
                 "about.short": "I create user-friendly and reusable APIs and web components that empower businesses of all sizes.",
                 "about.aboutMe": "About Me",
                 "about.paragraphOne": "Have you ever wanted to turn your D&D campaign into a web app? That's exactly what sparked my coding journey back in 2017. Since then, I've honed my skills as a software engineer, building complex technical solutions for the UK civil service. From implementing new user-facing designs to creating a new caching pattern, I've had the opportunity to deliver excellent results to amazing clients. Now, I'm eager to leverage my full-stack expertise as a freelance polyglot developer with experience in a wide range of frontend, backend and database technologies.",
-                "about.paragraphTwo": "Outside of code, I find balance through strength training, experimenting with new recipes in the kitchen, and creating pixel art. These passions fuel my creativity and problem-solving skills, which I bring to every project."
+                "about.paragraphTwo": "Outside of code, I find balance through strength training, experimenting with new recipes in the kitchen, and creating pixel art. These passions fuel my creativity and problem-solving skills, which I bring to every project.",
+
+                "experience.2025": "2025",
+                "experience.2024": "2024",
+                "experience.2023": "2023",
+                "experience.2022": "2022",
+
+                "experience.2021": "2021",
+                "experience.2020": "2020",
+                "experience.2019": "2019",
+                "experience.2018": "2018",
+                "experience.2017": "2017",
             }
         }
     },
