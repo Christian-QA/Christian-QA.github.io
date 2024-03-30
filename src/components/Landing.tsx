61b4338591c0be0d1d9f05125edaf72bbf04f5a4
Christian Redfern <credfernbus@gmail.com>
2024-03-30 21:07:15 +0000

    Sorted grid, easier to make responsive than the scatter idea

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -13,38 +13,45 @@ const Landing: React.FC = () => {
 
     return (
         <div className="section-content">
-            <div className="language-image first">
-                <img src="/src/assets/language-logos/scala-ar21.svg" alt="scala"/>
-            </div>
-            <div className="language-image second">
-                <img src="/src/assets/language-logos/java-ar21.svg" alt="java"/>
-            </div>
-            <div className="language-image third">
-                <img src="/src/assets/language-logos/kotlinlang-ar21.svg" alt="kotlin"/>
-            </div>
-            <div className="language-image fourth">
-                <img src="/src/assets/language-logos/javascript-ar21.svg" alt="javascript"/>
-            </div>
-            <div className="language-image fifth">
-                <img src="/src/assets/language-logos/dartlang-official.svg" alt="dart"/>
-            </div>
-            <div className="language-image sixth">
-                <img src="/src/assets/language-logos/typescriptlang-icon.svg" alt="typescript"/>
-            </div>
-            <div className="language-image seventh">
-                <img src="/src/assets/language-logos/w3_html5-ar21.svg" alt="html5"/>
-            </div>
-            <div className="language-image eighth">
-                <img src="/src/assets/language-logos/w3_css-ar21.svg" alt="css"/>
-            </div>
-            {/*    TODO - Add swift for loyal stamper iOS version when developed, and Python   */}
-
-
             <div className="landing-container">
                 <div className="landing-text">
                     <h2>{t('landing.title')}</h2>
                     <p className="subtitle">{t('landing.subtitle')}</p>
                     <p className="tagline">{t('landing.tagline')}</p>
+                </div>
+                <div className="language-grid" id="languages">
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/scala-ar21.svg" alt="scala" className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/java-ar21.svg" alt="java" className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/kotlinlang-ar21.svg" alt="kotlin"
+                             className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/javascript-ar21.svg" alt="javascript"
+                             className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/dartlang-official.svg" alt="dart"
+                             className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/typescriptlang-icon.svg" alt="typescript"
+                             className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/w3_html5-ar21.svg" alt="html" className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/w3_css-ar21.svg" alt="css" className="language-image"/>
+                    </div>
+                    <div className="language-container">
+                        <img src="/src/assets/language-logos/python-official.svg" alt="css" className="language-image"/>
+                    </div>
+                    {/*    TODO - Add swift for loyal stamper iOS version when developed   */}
                 </div>
             </div>
         </div>
