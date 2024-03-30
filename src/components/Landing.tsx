6a36ffc212d8db8034b48d2ba6a6a98942fd564e
Christian Redfern <credfernbus@gmail.com>
2024-03-30 18:55:21 +0000

    Was working on image grid, but gonna revisit image scatter first

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -11,7 +11,6 @@ const Landing: React.FC = () => {
     const { t } = useTranslation();
 
 
-
     return (
         <div className="section-content">
             <div className="landing-container">
@@ -20,25 +19,25 @@ const Landing: React.FC = () => {
                     <p className="subtitle">{t('landing.subtitle')}</p>
                     <p className="tagline">{t('landing.tagline')}</p>
                 </div>
-            </div>
-            <div className="language-containter">
-                <div className="row-grid">
-                    <div className="column-grid">
-                        <img src="/src/assets/language-logos/scala-ar21.svg" alt="scala"/>
-                        <img src="/src/assets/language-logos/java-ar21.svg" alt="java"/>
-                        <img src="/src/assets/language-logos/kotlinlang-ar21.svg" alt="typescript"/>
-                    </div>
-                    <div className="column-grid">
-                        <img src="/src/assets/language-logos/javascript-ar21.svg" alt="javascript"/>
-                        <img src="/src/assets/language-logos/dartlang-official.svg" alt="dart"/>
-                    </div>
-                    <div className="column-grid">
-                        <img src="/src/assets/language-logos/typescriptlang-icon.svg" alt="javascript"/>
-                        <img src="/src/assets/language-logos/python-official.svg" alt="python"/>
-                    </div>
-                    <div className="column-grid">
-                        <img src="/src/assets/language-logos/w3_html5-ar21.svg" alt="html"/>
-                        <img src="/src/assets/language-logos/w3_css-ar21.svg" alt="css"/>
+                <div className="language-grid" id="languages">
+                    <div className="row-grid">
+                        <div className="column-grid">
+                            <div className="language-images"><img src="/src/assets/language-logos/scala-ar21.svg" alt="scala"/></div>
+                            <img src="/src/assets/language-logos/java-ar21.svg" alt="java"/>
+                        </div>
+                        <div className="column-grid">
+                            <img src="/src/assets/language-logos/kotlinlang-ar21.svg" alt="kotlin"/>
+                            <img src="/src/assets/language-logos/javascript-ar21.svg" alt="javascript"/>
+                        </div>
+                        <div className="column-grid">
+                            <img src="/src/assets/language-logos/dartlang-official.svg" alt="dart"/>
+                            <img src="/src/assets/language-logos/typescriptlang-icon.svg" alt="typescript"/>
+                        </div>
+                        <div className="column-grid">
+                            <img src="/src/assets/language-logos/w3_html5-ar21.svg" alt="html"/>
+                            <img src="/src/assets/language-logos/w3_css-ar21.svg" alt="css"/>
+                        </div>
+                    {/*    TODO - Add swift for loyal stamper iOS version when developed   */}
                     </div>
                 </div>
             </div>
