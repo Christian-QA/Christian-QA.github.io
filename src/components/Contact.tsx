4b31e910e0c54ea743c97fd00984111feb7a50ae
Christian Redfern <credfernbus@gmail.com>
2024-03-30 12:24:38 +0000

    Added confirmation message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,22 @@
-import React, {useRef} from 'react';
-import {useTranslation} from "react-i18next";
+import React, { useRef } from 'react';
+import { useTranslation } from "react-i18next";
 import emailjs from '@emailjs/browser';
 
 const Contact: React.FC = () => {
     const { t } = useTranslation();
+    const form = useRef<HTMLFormElement>(null);
+    const confirmation = useRef<HTMLDivElement>(null);
 
-    const form = useRef();
-
-    const sendEmail = (e) => {
+    const sendEmail = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-
         emailjs
             .sendForm('service_kcq9eve', 'template_a79ze1b', form.current, {
                 publicKey: '4C9NhAMkXGZnLE75c',
             })
             .then(
                 () => {
+                    if (form.current) form.current.style.display = "none";
+                    if (confirmation.current) confirmation.current.style.display = "flex";
                     console.log('SUCCESS!');
                 },
                 (error) => {
@@ -27,16 +28,19 @@ const Contact: React.FC = () => {
     return (
         <div className="section-content">
             <h2 className="header">{t("contact.header")}</h2>
+            <div className="confirmation-container" style={{ display: "none" }} ref={confirmation}>
+                <div className="confirmation-text">
+                    <h3>Thank you for your message!</h3>
+                    <p>I'll get back to you soon</p>
+                </div>
+            </div>
             <form className="contact-form" id="contact" ref={form} onSubmit={sendEmail}>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="from_name" required/>
-
+                <input type="text" id="name" name="from_name" required />
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="from_email" required/>
-
+                <input type="email" id="email" name="from_email" required />
                 <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" rows={15} required/>
-
+                <textarea id="message" name="message" rows={15} required />
                 <button type="submit" value="Send">Send Message</button>
             </form>
         </div>
