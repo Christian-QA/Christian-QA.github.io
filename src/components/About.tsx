724451984a7ddfc06cfcbff7c56d917455fff43d
Christian Redfern <credfernbus@gmail.com>
2024-04-01 14:07:21 +0100

    Working on about section layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import Messages from "../lang/en.tsx";
-import 'w3-css/w3.css';
 import '../styling/About.css'
 
 i18n
@@ -15,45 +14,21 @@ const About: React.FC = () => {
     return (
         <div className="section-content">
             <h2 className="header">{t("about.header")}</h2>
-            <div className="w3-main w3-padding-large">
-            <div className="w3-padding-32 w3-content" id="portfolio">
-                <nav className="w3-sidebar w3-hide-medium w3-hide-small image-container">
-                    <div className="bgimg"><img src="/src/assets/portfolio_image_trim.png" alt="me"/></div>
-                </nav>
-
-                <div className="w3-content w3-justify w3-text-grey w3-padding-32" id="about">
-                    <h2>About</h2>
-                    <hr className="w3-opacity"/>
-                    <p>{t('about.paragraphOne')}</p>
-                    <br></br>
-                    <p>{t('about.paragraphTwo')}</p>
-                    <div className="w3-row w3-center w3-dark-grey w3-padding-16 w3-section">
-                        <div className="w3-quarter w3-section">
-                            <span className="w3-xlarge">14+</span>
-                            <br/>
-                            Partners
-                        </div>
-                        <div className="w3-quarter w3-section">
-                            <span className="w3-xlarge">55+</span>
-                            <br/>
-                            Projects Done
-                        </div>
-                        <div className="w3-quarter w3-section">
-                            <span className="w3-xlarge">89+</span>
-                            <br/>
-                            Happy Clients
-                        </div>
-                        <div className="w3-quarter w3-section">
-                            <span className="w3-xlarge">150+</span>
-                            <br/>
-                            Meetings
-                        </div>
+            <div className="about-section">
+                <div className="image-container">
+                    <img src="/src/assets/portfolio_image.jpg" alt="me" className="portfolio-image"></img>
+                    <div className="text-overlay">
+                        {/*Do I still want a text overlay?*/}
                     </div>
-                    <button className="w3-button w3-light-grey w3-padding-large w3-section">
-                        <i className="fa fa-download"></i> Download Resume
-                    </button>
                 </div>
-            </div>
+                <div className="text-container">
+                    <div className="about-me"><h1>{t('about.aboutMe')}</h1></div>
+                    <div className="about-story">
+                        <p>{t('about.paragraphOne')}</p>
+                        <br></br>
+                        <p>{t('about.paragraphTwo')}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
